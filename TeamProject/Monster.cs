9b7a21e74a5d34f9b61357e030d7a601e87c87e1
suho1210 <tngh1313@gmail.com>
2025-07-14 21:41:02 +0900

    monster class 작성.
    아직 오류 있으나 해결 못함.
    어디서 생기는 오류인지 파악못함..

--- TeamProject/Monster.cs
+++ TeamProject/Monster.cs
@@ -1,10 +1,53 @@
-﻿namespace TeamProject
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TeamProject
 {
-    internal class Program
+    public class Monster
     {
-        static void Main(string[] args)
+        public string Name;
+        public int Level;
+        public int MaxHp;
+        public int Hp;
+        public int Atk;
+        public int Def;
+        public string Description;
+
+
+        public Monster(string name, int level, int maxhp, int atk, int def, string description)
         {
-            SceneManager.Instance.StartScene();
+            Name = name;
+            Level = level;
+            MaxHp = maxhp;
+            Hp = maxhp;
+            Atk = atk;
+            Def = def;
+            Description = description;
         }
+
+        public void DamageTaken(int damage)
+        {
+            Hp -= damage;
+            if (Hp < 0)
+                Hp = 0;
+        }
+
+       //몬스터 죽은후 상태(dead) + 나중에 회색처리 예정
+        public string Status()
+        {
+            return Hp <= 0 ? $"{Name} (Dead)" : $"{Name} (HP: {Hp})";
+        }
+
+
+        List<Monster> monsters = new List<Monster>();
+
+        void CreateMonsters()
+        {
+            monsters.Add(new Monster("코스믹 슬라임", 2, 15, 6, 1, "우주에 있는 가장 평범한 몬스터."));
+            monsters.Add(new Monster("네뷸라 기생충", 5, 25, 10, 3, "성운(네뷸라) 구름 속을 유영하는 거대 기생충."));
+            monsters.Add(new Monster("궁허충", 3, 10, 7, 0, "빠르고 공격적인 벌레."));
+            monsters.Add(new Monster("외계인 병사", 1, 8, 4, 1, "우주 말단 병사."));
+        }
+
+        //오류가 어디서 나는걸까..
     }
 }
