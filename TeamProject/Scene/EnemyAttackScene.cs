adfc1c6cdc17765b0cfd551bc7470c7c232b22cc
wong1125 <wong1125@naver.com>
2025-07-15 19:03:51 +0900

    [Feat] 몬스터가 순차적으로 공격

--- TeamProject/Scene/EnemyAttackScene.cs
+++ TeamProject/Scene/EnemyAttackScene.cs
@@ -16,44 +16,85 @@ namespace TeamProject
             options.Add("0. 다음");
             optionsLen = options.Count;
 
-            monster = new Monster("대포미니언", 99, 124, 10, 5, "");
-
+            monster1 = new Monster("대포미니언", 1, 124, 10, 5, "");
+            monster2 = new Monster("대포미니언", 2, 124, 10, 5, "");
+            monster3 = new Monster("대포미니언", 3, 124, 10, 5, "");
+            monsters.Add(monster1);
+            monsters.Add(monster2);
+            monsters.Add(monster3);
         }
+        
         float initialHp = Player.Instance.Hp;
         StringBuilder sb;
         int selOptions = 0;
 
         //받아와야 하는거
         //현재 살아있는 몬스터 목록
-        Monster monster;
+        Monster monster1;
+        Monster monster2;
+        Monster monster3;
+        List<Monster> monsters = new List<Monster>();
 
 
         public override void Render()
         {
-            sb.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow; // 출력 색 지정
-            sb.AppendLine("Battle!!");
-            Console.Write(sb.ToString());
-            Console.ResetColor();// 출력 색 초기화
 
-            sb.Clear();
-            sb.AppendLine($"LV.{monster.Level} {monster.Name} 의 공격!");
-            sb.Append($"{Player.Instance.Name} 을(를) 맞췄습니다.  ");
-            sb.AppendLine($"[데미지:10]");
-            sb.AppendLine();
-
-            sb.AppendLine($"Lv.{Player.Instance.Lv} {Player.Instance.Name}");
-            sb.AppendLine($"Hp {initialHp} -> {Player.Instance.Hp}");
-            for (int i = 0; i < optionsLen; i++)
+            foreach (Monster monster in monsters)
             {
-                if (selOptions == i) sb.Append("▶ ");
-                else sb.Append("　 ");
-                sb.AppendLine(options[i]);
+
+                StringBuilder sb0 = new StringBuilder();
+                for (int i = 0; i < 36; i++)
+                {
+                    for (int j = 0; j < 120; j++)
+                    {
+                        sb0.Append(" ");
+                    }
+                    sb0.Append("\n");
+                }
+                string tmpS = sb0.ToString();
+
+                // 깜빡임 줄이기 위해 빈공간으로 덮어 쓰기
+                Console.SetCursorPosition(0, 0);
+
+
+                //if(monster.isDied) continue;
+
+                sb.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow; // 출력 색 지정
+                sb.AppendLine("Battle!!");
+                Console.Write(sb.ToString());
+                Console.ResetColor();// 출력 색 초기화
+
+                sb.Clear();
+                sb.AppendLine($"LV.{monster.Level} {monster.Name} 의 공격!");
+
+                sb.Append($"{Player.Instance.Name} 을(를) 맞췄습니다.  ");
+                sb.AppendLine($"[데미지:10]");
+                sb.AppendLine();
+
+                Player.Instance.Hp -= 10;
+
+
+                sb.AppendLine($"Lv.{Player.Instance.Lv} {Player.Instance.Name}");
+                sb.AppendLine($"Hp {initialHp} -> {Player.Instance.Hp}");
+
+                initialHp = Player.Instance.Hp;
+
+                for (int i = 0; i < optionsLen; i++)
+                {
+                    if (selOptions == i) sb.Append("▶ ");
+                    else sb.Append("　 ");
+                    sb.AppendLine(options[i]);
+                }
+                sb.AppendLine();
+                sb.AppendLine("이동: 방향키, 선택: z, 돌아가기: x");
+                Console.Write(sb.ToString());
+                SceneControl();
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write(tmpS);
+
             }
-            sb.AppendLine();
-            sb.AppendLine("이동: 방향키, 선택: z, 돌아가기: x");
-            Console.Write(sb.ToString());
-            SceneControl();
         }
 
         protected override void SceneControl()
@@ -74,10 +115,10 @@ namespace TeamProject
                     break;
                 case ConsoleKey.Z:
 
-                    // if (Player.Instance.Hp <= 0) SceneManager.Instance.SetSceneState = SceneManager.SceneState.StartScene; //게임 오버 씬
-                    // else if (행동하지 않은 몬스터가 있으면) SceneManager.Instance.SetSceneState = SceneManager.SceneState.EnemyAttckScene // 다음 몬스터 행동
-                    // else if (행동하지 않은 몬스터는 없지만 살아있는 몬스터가 있으면) SceneManager.Instance.SetSceneState = SceneManager.SceneState.PlayerAttackScene
-                    // else (행동하지 않은 몬스터도 없고 살아있는 몬스터도 없으면 VictoryScene
+                    // if (Player.Instance.Hp <= 0) //게임 오버 씬
+                    // else if (살아있는 몬스터가 있으면) SceneManager.Instance.SetSceneState = SceneManager.SceneState.PlayerAttackScene
+                    // else (살아있는 몬스터가 없으면) //승리 씬
+
                     break;
                 case ConsoleKey.X:
                     break;
