21fe59638c3f2161eb98804242ef100c543dad76
suho1210 <tngh1313@gmail.com>
2025-07-15 16:39:19 +0900

    Update MonsterLibrary.cs

--- TeamProject/MonsterLibrary.cs
+++ TeamProject/MonsterLibrary.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace TeamProject
 {
-    public static class MonsterLibrary
+/*
+    public class MonsterLibrary
     {
+        private List<Monster> monsters;
 
+        public MonsterLibrary()
+        {
+            monsters = new List<Monster>();
+            CreateMonsters();  
+        }
+
+        void CreateMonsters()
+        {
+            // 고블린
+            monsters.Add(new Monster());
+            monsters[0].Name = "고블린";
+            monsters[0].Level = 1;
+            monsters[0].MaxHp = 20; 
+            monsters[0].Hp = 20;
+            monsters[0].Atk = 6;
+            monsters[0].Def = 2;
+            monsters[0].Description = "숲 속에 자주 출몰하는 약한 몬스터";
+            //
+            monsters.Add(new Monster());
+            monsters[1].Name = "고블린";
+            monsters[1].Level = 5;
+            monsters[1].MaxHp = 25; 
+            monsters[1].Hp = 25;
+            monsters[1].Atk = 6;
+            monsters[1].Def = 2;
+            monsters[1].Description = "숲 속에 자주 출몰하는 약한 몬스터";
+
+        }
     }
-
-
 }
+*/
