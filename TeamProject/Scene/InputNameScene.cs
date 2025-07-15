5b0b6096749820821387595c2fc8b94c96223014
gyro1515 <gyro1515@naver.com>
2025-07-15 09:46:52 +0900

    [ Feat ] 이름 입력 씬 -> 게임 인트로 씬(연출 맛보기) 구현

--- TeamProject/Scene/InputNameScene.cs
+++ TeamProject/Scene/InputNameScene.cs
@@ -14,6 +14,7 @@ namespace TeamProject
             sbName = new StringBuilder();
             CreatScene();
             state = InputState.Input;
+
         }
         public enum InputState
         {
@@ -23,73 +24,56 @@ namespace TeamProject
 
         StringBuilder sb;
         StringBuilder sbName;
-        public override void Render()
-        {
-            sb.Clear();
-            sb.Append("이름을 입력해주세요. (Enter로 완료): ");
-            sb.AppendLine(sbName.ToString());
-            Console.Write(sb);
-        }
-
+        
         public override void Update()
         {
-            switch (state)
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    //state = InputState.NextScene;
+                    Console.WriteLine("1초 뒤 다음 씬으로");
+                    Console.CursorVisible = false;
+                    Thread.Sleep(1000);
+                    SceneManager.Instance.SetSceneState = SceneManager.SceneState.GameIntroScene;
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && sbName.Length > 0)
+                {
+                    sbName.Length--;
+                }
+                else if (!char.IsControl(keyInfo.KeyChar)) // 유효한 문자만
+                {
+                    sbName.Append(keyInfo.KeyChar);
+                }
+
+            }
+            /*switch (state)
             {
                 case InputState.Input:
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                        if (keyInfo.Key == ConsoleKey.Enter)
-                        {
-                            state = InputState.NextScene;
-                        }
-                        else if (keyInfo.Key == ConsoleKey.Backspace && sbName.Length > 0)
-                        {
-                            sbName.Length--;
-                        }
-                        else if (!char.IsControl(keyInfo.KeyChar)) // 유효한 문자만
-                        {
-                            sbName.Append(keyInfo.KeyChar);
-                        }
-
-                        /*switch (keyInfo.Key)
-                        {
-                            case ConsoleKey.UpArrow:
-                                //Console.WriteLine("↑ 위쪽 방향키 입력됨");
-                                break;
-                            case ConsoleKey.DownArrow:
-                                //Console.WriteLine("↓ 아래쪽 방향키 입력됨");
-                                break;
-                            case ConsoleKey.LeftArrow:
-                                //Console.WriteLine("← 왼쪽 방향키 입력됨");
-                                break;
-                            case ConsoleKey.RightArrow:
-                                //Console.WriteLine("→ 오른쪽 방향키 입력됨");
-                                break;
-                            case ConsoleKey.Escape:
-                                //Console.WriteLine("종료합니다.");
-                                break;
-                            case ConsoleKey.Z:
-                                //Console.WriteLine("z");
-                                break;
-                            case ConsoleKey.X:
-                                //Console.WriteLine("x");
-                                break;
-                            default:
-                                //Console.WriteLine($"다른 키 입력됨: {keyInfo.Key}"
-                                break;
-                        }*/
-                    }
+                    
                     break;
                 case InputState.NextScene:
                     Console.WriteLine("1초 뒤 복구");
                     Thread.Sleep(1000);
                     state = InputState.Input;
-                    //SceneManager.Instance.SetSceneState = SceneManager.SceneState.StartScene;
+                    //SceneManager.Instance.SetSceneState = SceneManager.SceneState.;
                     break;
                 default:
                     break;
-            }
+            }*/
+        }
+        public override void Render()
+        {
+            sb.Clear();
+            sb.Append("이름을 입력해주세요. (Enter로 완료): ");
+            /*Console.Write(sb);
+            Console.Clear();
+            string name = Console.ReadLine();
+            sb.AppendLine(name);*/
+            //sb.Append(sbName.ToString());
+            sb.Append(sbName);
+            Console.Write(sb);
         }
         void CreatScene()
         {
