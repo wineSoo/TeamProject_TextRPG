f5fe9ed2539f36b81daf6e1669edd1ba0fbf34b4
gyro1515 <gyro1515@naver.com>
2025-07-16 20:34:23 +0900

    [Fix] 던전 층 수 출력하고, 층 수 올리기

--- TeamProject/Scene/WinEndScene.cs
+++ TeamProject/Scene/WinEndScene.cs
@@ -29,7 +29,7 @@ namespace TeamProject
 
         void ResultCalculator()
         {
-            Player.Instance.DungeonFloor++;
+            //Player.Instance.DungeonFloor++; // 출력할때 변경하기
             pastLevel = Player.Instance.Lv;
             pastExp= Player.Instance.Exp;
 
@@ -55,7 +55,7 @@ namespace TeamProject
             sb.AppendLine();
             sb.AppendLine("Victory");
             sb.AppendLine();
-            sb.AppendLine($"{Player.Instance.DungeonFloor}층 던전에서 몬스터 {totalMonsterNumber}마리를 잡았습니다.");
+            sb.AppendLine($"{Player.Instance.DungeonFloor++}층 던전에서 몬스터 {totalMonsterNumber}마리를 잡았습니다.");
             sb.AppendLine();
             sb.AppendLine("[캐릭터 정보]");
             if (isLevelUp) sb.AppendLine($"Lv.{pastLevel} {player.Name} -> Lv.{player.Lv} {player.Name}");
