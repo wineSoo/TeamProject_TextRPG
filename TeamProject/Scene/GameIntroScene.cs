59987658ef9ff0ea6bba82c4980327c35f187f78
gyro1515 <gyro1515@naver.com>
2025-07-16 15:11:36 +0900

    [Fix] 인트로 씬 수정

--- TeamProject/Scene/GameIntroScene.cs
+++ TeamProject/Scene/GameIntroScene.cs
@@ -10,22 +10,23 @@ namespace TeamProject
     {
         public GameIntroScene()
         {
-            title = "게임 인트로 씬은 우선 한 글자 씩 출력되다가, 모든 내용이 출력된 후 넘어가도록 세팅하겠습니다.\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠★★★★＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
-            title += "＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠\n";
+            title = "██████╗ ███████╗███╗   ███╗ ██████╗ ███╗   ██╗\n";
+            title += "██╔══██╗██╔════╝████╗ ████║██╔═══██╗████╗  ██║\n";
+            title += "██║  ██║█████╗  ██╔████╔██║██║   ██║██╔██╗ ██║\n";
+            title += "██║  ██║██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║\n";
+            title += "██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║\n";
+            title += "╚═════╝ ╚══════╝╚═╝     ╚═╝╚═════╝ ╚═╝  ╚═══╝\n";
+            title += "██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ ███████╗\n";
+            title += "██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝\n";
+            title += "███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝███████╗\n";
+            title += "██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗╚════██║\n";
+            title += "██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║███████║\n";
+            title += "╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝\n";
             sb = new StringBuilder();
         }
         int titleIdx = -2;
         string title;
-        int speed = 100; // 숫자 높을수록 느리게 출력됩니다.
+        int speed = 150; // 숫자 높을수록 느리게 출력됩니다.
         StringBuilder sb;
         // 0.3초마다 한 글자 씩 출력 되도록
 
@@ -34,7 +35,7 @@ namespace TeamProject
             sb.Clear();
             if(titleIdx < title.Length)
             {
-                titleIdx += 10;
+                titleIdx += 20;
                 if(titleIdx > title.Length)
                 {
                     titleIdx = title.Length;
@@ -47,9 +48,10 @@ namespace TeamProject
             }
             if(titleIdx == title.Length) // 다 출력 했다면
             {
-                sb.Append("\n아무 키를 누르면 다음 씬으로 넘어갑니다.(테스트용, 구현 다했으면 자동으로 넘어감)");
+                //sb.Append("\n아무 키를 누르면 다음 씬으로 넘어갑니다.(테스트용, 구현 다했으면 자동으로 넘어감)");
                 Console.WriteLine(sb.ToString());
-                SceneControl(); // 키입력 대기 = 화면 멈추기
+                //SceneControl(); // 키입력 대기 = 화면 멈추기
+                Thread.Sleep(3000);
                 SceneManager.Instance.SetSceneState = SceneManager.SceneState.JobSelectScene;
             }
             else // 출력할 게 남았다면
